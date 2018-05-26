1453f468634af7255cb7737136a7d9703f12c926
Anthony Elijah <lionnet.ant@gmail.com>
2018-05-26 09:00:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,38 +23,37 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
   
-  # pass user input to board
+  # turn user input as board corresponding index
   def input_to_index(user_input)
     user_input.to_i - 1
   end
   
+  # put user input in the board array
   def move(index, player_token = "X")
     @board[index] = player_token
   end
   
+  # check if given user move is taken
   def position_taken?(index)
   !(@board[index] ==  " " || @board[index] == nil)
   end
   
+  # check if give user input position is untaken and between 0-8 
   def valid_move?(index)
   index.between?(0,8) && !position_taken?(index)
   end
   
-  def turn
-    puts "Enter your position (1-9)"
-    index = gets.strip
-    input_to_index(index)
-    valid_move?(index) ? move(index) : turn
-  end
-  
+  # return turns that have been played
   def turn_count
     @board.count {|x| x == "X" || x == "O" }
   end
   
+  # return current player regarding turn count
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
   
+  # get the user input position and check if valid, reask if not
   def turn 
     puts "Enter your position (1-9):"
     user_input = gets.strip
@@ -62,22 +61,27 @@ class TicTacToe
     valid_move?(index) ? move(index, value = current_player) && display_board : turn
   end
   
+  # check if won move has been made
   def won?
     WIN_COMBINATIONS.find { |win| @board[win[0]] == @board[win[1]] && @board[win[1]] == @board[win[2]] && position_taken?(win[0])}
   end
   
+  # check if board is full
   def full?
     return @board.all? { |value| value == "X" || value == "O"}
   end
   
+  # check if its a draw (no one won)
   def draw?
     full? && !won?
   end
   
+  # check if game is over no matter which way
   def over?
     won? || full?
   end
   
+  # give the winner value
   def winner
     if winning_combo = won?
       @board[winning_combo.first]
